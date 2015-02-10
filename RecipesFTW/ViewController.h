d82be986a99198cd952158164f8b95a8efdcd815
Zhn <zhukov.ever@gmail.com>
2015-02-10 22:35:08 +0500

    add: empty project

---
+++ RecipesFTW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecipesFTW
+//
+//  Created by Zhn on 10/02/2015.
+//  Copyright (c) 2015 Zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
